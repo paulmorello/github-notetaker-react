1f30eb2dd6f5e14e9b5c9619b3f792c7d9bd6897
paulmorello <paulmorello1@gmail.com>
2016-12-30 20:04:08 +1100

    updated classes based on new version of react

--- public/bundle.js
+++ public/bundle.js
@@ -24417,8 +24417,6 @@
 	  value: true
 	});
 
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 	var _react = __webpack_require__(1);
 
 	var _react2 = _interopRequireDefault(_react);
@@ -24429,47 +24427,29 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	var Main = function Main(_ref) {
+	  var history = _ref.history,
+	      children = _ref.children;
 
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var Main = function (_React$Component) {
-	  _inherits(Main, _React$Component);
-
-	  function Main() {
-	    _classCallCheck(this, Main);
-
-	    return _possibleConstructorReturn(this, (Main.__proto__ || Object.getPrototypeOf(Main)).apply(this, arguments));
-	  }
-
-	  _createClass(Main, [{
-	    key: 'render',
-	    value: function render() {
-	      return _react2.default.createElement(
+	  return _react2.default.createElement(
+	    'div',
+	    { className: 'main-container' },
+	    _react2.default.createElement(
+	      'nav',
+	      { className: 'navbar navbar-default', role: 'navigation' },
+	      _react2.default.createElement(
 	        'div',
-	        { className: 'main-container' },
-	        _react2.default.createElement(
-	          'nav',
-	          { className: 'navbar navbar-default', role: 'navigation' },
-	          _react2.default.createElement(
-	            'div',
-	            { className: 'col-sm-7 col-sm-offset-2', style: { marginTop: 15 } },
-	            _react2.default.createElement(_SearchGithub2.default, { history: this.props.history })
-	          )
-	        ),
-	        _react2.default.createElement(
-	          'div',
-	          { className: 'container' },
-	          this.props.children
-	        )
-	      );
-	    }
-	  }]);
-
-	  return Main;
-	}(_react2.default.Component);
+	        { className: 'col-sm-7 col-sm-offset-2', style: { marginTop: 15 } },
+	        _react2.default.createElement(_SearchGithub2.default, { history: history })
+	      )
+	    ),
+	    _react2.default.createElement(
+	      'div',
+	      { className: 'container' },
+	      children
+	    )
+	  );
+	};
 
 	exports.default = Main;
 
@@ -24575,42 +24555,19 @@
 	  value: true
 	});
 
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 	var _react = __webpack_require__(1);
 
 	var _react2 = _interopRequireDefault(_react);
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var Home = function (_React$Component) {
-	  _inherits(Home, _React$Component);
-
-	  function Home() {
-	    _classCallCheck(this, Home);
-
-	    return _possibleConstructorReturn(this, (Home.__proto__ || Object.getPrototypeOf(Home)).apply(this, arguments));
-	  }
-
-	  _createClass(Home, [{
-	    key: "render",
-	    value: function render() {
-	      return _react2.default.createElement(
-	        "h2",
-	        { className: "text-center" },
-	        "Search By Github Username Above"
-	      );
-	    }
-	  }]);
-
-	  return Home;
-	}(_react2.default.Component);
+	var Home = function Home() {
+	  return _react2.default.createElement(
+	    "h2",
+	    { className: "text-center" },
+	    "Search By Github Username Above"
+	  );
+	};
 
 	exports.default = Home;
 
