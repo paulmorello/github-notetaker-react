959a883011fb7cfccbdffd527952838bd0ed585b
paulmorello <paulmorello1@gmail.com>
2016-12-30 15:00:21 +1100

    Implemented firebase with react-fire for notes section

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,30 +3,42 @@ var Router = require('react-router');
 var Repos = require('./github/Repos');
 var UserProfile = require('./github/UserProfile');
 var Notes = require('./notes/Notes');
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
 
 var Profile = React.createClass({
+  mixins: [ReactFireMixin],
   getInitialState() {
     return {
       notes: [1,2,3],
       bio: {
-        name: 'Paul Morello'
+        name: 'facebook'
       },
       repos: ['a', 'b', 'c'],
     }
   },
 
+  componentDidMount: function() {
+    this.ref = new Firebase("https://github-notetaker-react-57636.firebaseio.com/")
+    var childRef = this.ref.child(this.props.params.username);
+    this.bindAsArray(childRef, 'notes');
+  },
+
+  componentWillUnmount: function() {
+    this.unbind('notes');
+  },
+
   render: function() {
-    console.log(this.props)
     return (
       <div className="row">
         <div className="col-md-4">
           <UserProfile username={this.props.params.username} bio={this.state.bio}/>
         </div>
         <div className="col-md-4">
-          <Repos repos={this.state.repos}/>
+          <Repos username={this.props.params.username} repos={this.state.repos} />
         </div>
         <div className="col-md-4">
-          <Notes notes={this.state.notes}/>
+          <Notes username={this.props.params.username} notes={this.state.notes}/>
         </div>
       </div>
     )
